f671493de0fd5aad94c4777717edad51fbc2c835
Lahir-Sai-Vignesh <lahirsaivignesh3@gmail.com>
2024-05-03 15:01:36 +0530

    Initial Commit

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,16 @@
-import './App.css';
+import {BrowserRouter,Routes,Route} from 'react-router-dom';
+import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
 
 function App() {
   return (
     <div className="App">
-      <h1>Twitter</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path='/' element=<Home/>/>
+          <Route path='/login' element=<Login /> />
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
